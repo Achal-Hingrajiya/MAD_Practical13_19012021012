6e3a83757c3cbc6c26a8177230a878092fbeb2b4
Achal-H14 <apcomp1410@gmail.com>
2021-12-05 00:26:48 +0530

    Completed functionality: Loading of an Incoming SMS to the listview

--- app/src/main/java/com/example/practical13_19012021012/InboxActivity.kt
+++ app/src/main/java/com/example/practical13_19012021012/InboxActivity.kt
@@ -1,11 +1,25 @@
 package com.example.practical13_19012021012
 
+import android.Manifest
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Telephony
+import android.widget.ListView
+import androidx.core.app.ActivityCompat
+import com.example.practical13_19012021012.src.adapters.InboxSmsAdapter
+import com.example.practical13_19012021012.src.sms_info.InboxMessages
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class InboxActivity : AppCompatActivity() {
+
+    lateinit var lvAdapter : InboxSmsAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inbox)
@@ -24,5 +38,60 @@ class InboxActivity : AppCompatActivity() {
             }
         }
 
+        val lvInboxSms = findViewById<ListView>(R.id.lv_inbox_sms)
+
+        lvAdapter = InboxSmsAdapter(this, InboxMessages.inboxSmsArr)
+        lvInboxSms.adapter = lvAdapter
+
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.RECEIVE_SMS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(
+                    Manifest.permission.RECEIVE_SMS,
+                    Manifest.permission.SEND_SMS,
+                    Manifest.permission.READ_SMS
+                ),
+                111
+            )
+        } else {
+            receiveMsg()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 111
+            && grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            receiveMsg()
+        }
+    }
+
+    private fun receiveMsg() {
+        val br = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                    for (sms in Telephony.Sms.Intents.getMessagesFromIntent(intent)) {
+
+                        val contactNo = sms.originatingAddress.toString()
+                        val body = sms.displayMessageBody.toString()
+                        val newSms = InboxMessages(contactNo, body)
+                        InboxMessages.addSMS(newSms)
+                        lvAdapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        }
+        registerReceiver(br, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
     }
 }
