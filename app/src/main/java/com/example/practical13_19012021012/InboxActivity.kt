6ff4dccf0e960e05ad24ca0611449bd22e2f2f20
Achal-H14 <apcomp1410@gmail.com>
2021-12-05 02:52:48 +0530

    Completed Practical13

--- app/src/main/java/com/example/practical13_19012021012/InboxActivity.kt
+++ app/src/main/java/com/example/practical13_19012021012/InboxActivity.kt
@@ -8,14 +8,18 @@ import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.Telephony
+import android.telephony.SmsManager
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
 import android.widget.ListView
 import android.widget.TimePicker
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import com.example.practical13_19012021012.src.adapters.InboxSmsAdapter
 import com.example.practical13_19012021012.src.sms_info.InboxMessages
+import com.example.practical13_19012021012.src.sms_info.OutboxMessages
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.android.material.textfield.TextInputEditText
@@ -82,6 +86,7 @@ class InboxActivity : AppCompatActivity() {
 
     private fun receiveMsg() {
         val br = object : BroadcastReceiver() {
+            @RequiresApi(Build.VERSION_CODES.N)
             override fun onReceive(context: Context?, intent: Intent?) {
 
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
@@ -90,7 +95,7 @@ class InboxActivity : AppCompatActivity() {
                         val contactNo = sms.originatingAddress.toString()
                         val body = sms.displayMessageBody.toString()
 
-
+                        showSmsDialog(contactNo, body)
                         val newSms = InboxMessages(contactNo, body)
                         InboxMessages.addSMS(newSms)
                         lvAdapter.notifyDataSetChanged()
@@ -101,49 +106,41 @@ class InboxActivity : AppCompatActivity() {
         registerReceiver(br, IntentFilter("android.provider.Telephony.SMS_RECEIVED"))
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun showSmsDialog(contactNo : String, smsBody : String) {
+        val dialogTitle = "New SMS Received"
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
 
-//    @RequiresApi(Build.VERSION_CODES.N)
-//    private fun showEditNoteDialog(sms: InboxMessages) {
-//        val dialogTitle = "SMS Received"
-//        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
-//        builder.setTitle(dialogTitle)
-//
-//        val customLayout: View = LayoutInflater.from(this).inflate(R.layout.new_sms_dialog, null)
-//
-//
-////
-//        title.setText(note.title)
-//        subtitle.setText(note.subTitle)
-//        desc.setText(note.Description)
-//        reminderSwitch.isChecked = note.isReminder
-//
-//        timePicker.hour = getHour(note.remindertime)
-//        timePicker.minute = getMinute(note.remindertime)
-//
-//        builder.setView(customLayout)
-//        builder.setPositiveButton(
-//            "Save Changes",
-//            DialogInterface.OnClickListener { dialog, which ->
-//
-//                val hour = timePicker.hour
-//                val minute = timePicker.minute
-//                val remTimeInMillis = getMillis(hour, minute)
-//                val modifiedTime = getCurrentDateTime(remTimeInMillis)
-//
-//                note.title = title.text.toString()
-//                note.subTitle = subtitle.text.toString()
-//                note.Description = desc.text.toString()
-//                note.isReminder = reminderSwitch.isChecked
-//                note.modifiedTime = modifiedTime
-//
-//
-//                notifyDataSetChanged()
-//
-//            })
-//
-//        val dialog: AlertDialog = builder.create()
-//        dialog.show()
-//
-//    }
+        val customLayout: View = LayoutInflater.from(this).inflate(R.layout.sms_card, null)
+
+        val tvContactNo = customLayout.findViewById<EditText>(R.id.tv_contact_no)
+        val tvSmsBody = customLayout.findViewById<EditText>(R.id.tv_sms_body)
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            tvContactNo.focusable = View.NOT_FOCUSABLE
+            tvSmsBody.focusable = View.NOT_FOCUSABLE
+
+        }
+        tvContactNo.isFocusableInTouchMode = false
+        tvContactNo.setBackgroundResource(android.R.color.transparent)
+        tvSmsBody.isFocusableInTouchMode = false
+        tvSmsBody.setBackgroundResource(android.R.color.transparent)
+
+        tvContactNo.setText(contactNo)
+        tvSmsBody.setText(smsBody)
+
+        builder.setView(customLayout)
+        builder.setPositiveButton(
+            "Close",
+            DialogInterface.OnClickListener { _, _ ->
+
+            })
+
+        val dialog: AlertDialog = builder.create()
+
+        dialog.show()
+
+    }
 
 }
