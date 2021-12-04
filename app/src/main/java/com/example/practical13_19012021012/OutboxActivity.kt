ed8357e5cdad8657f419619f1e587e031eceed3a
Achal-H14 <apcomp1410@gmail.com>
2021-12-05 02:08:08 +0530

    Added dialog onlcick of new sms button

--- app/src/main/java/com/example/practical13_19012021012/OutboxActivity.kt
+++ app/src/main/java/com/example/practical13_19012021012/OutboxActivity.kt
@@ -1,13 +1,22 @@
 package com.example.practical13_19012021012
 
 import android.Manifest
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telephony.SmsManager
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ListView
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
-import com.example.practical13_19012021012.src.adapters.InboxSmsAdapter
 import com.example.practical13_19012021012.src.adapters.OutboxSmsAdapter
 import com.example.practical13_19012021012.src.sms_info.InboxMessages
 import com.example.practical13_19012021012.src.sms_info.OutboxMessages
@@ -17,6 +26,7 @@ class OutboxActivity : AppCompatActivity() {
 
     lateinit var lvAdapter : OutboxSmsAdapter
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_outbox)
@@ -41,12 +51,6 @@ class OutboxActivity : AppCompatActivity() {
 
         val lvInboxSms = findViewById<ListView>(R.id.lv_outbox_sms)
 
-        OutboxMessages.addSMS(OutboxMessages("6355689874", "This is a testing sms."))
-        OutboxMessages.addSMS(OutboxMessages("6355689874", "This is a testing sms."))
-        OutboxMessages.addSMS(OutboxMessages("6355689874", "This is a testing sms."))
-        OutboxMessages.addSMS(OutboxMessages("6355689874", "This is a testing sms."))
-        OutboxMessages.addSMS(OutboxMessages("6355689874", "This is a testing sms."))
-        OutboxMessages.addSMS(OutboxMessages("6355689874", "This is a testing sms."))
 
         lvAdapter = OutboxSmsAdapter(this, OutboxMessages.outboxSmsArr)
         lvInboxSms.adapter = lvAdapter
@@ -66,6 +70,51 @@ class OutboxActivity : AppCompatActivity() {
                 111
             )
         }
+        val btnNewSms = findViewById<Button>(R.id.btn_new_sms)
+        btnNewSms.setOnClickListener {
+            showNewSmsDialog()
+        }
+    }
+
+
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun showNewSmsDialog() {
+        val dialogTitle = "Send SMS"
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+
+        val customLayout: View = LayoutInflater.from(this).inflate(R.layout.sms_card, null)
+
+
+        builder.setView(customLayout)
+        builder.setPositiveButton(
+            "Send",
+            DialogInterface.OnClickListener { dialog, which ->
+
+
+                val tvContactNo = customLayout.findViewById<EditText>(R.id.tv_contact_no)
+                val tvBody = customLayout.findViewById<EditText>(R.id.tv_sms_body)
+                val contactNo = tvContactNo.text.toString()
+                val body = tvBody.text.toString()
+
+
+                val sms = SmsManager.getDefault()
+
+                sms.sendTextMessage(contactNo, "Achal Hingrajiya", body, null, null)
+
+                Toast.makeText(this, "SMS Sent",Toast.LENGTH_SHORT).show()
+
+                val newSms = OutboxMessages(contactNo, body)
+                OutboxMessages.addSMS(newSms)
+
+                lvAdapter.notifyDataSetChanged()
+
+            })
+
+        val dialog: AlertDialog = builder.create()
+
+        dialog.show()
 
     }
 }
